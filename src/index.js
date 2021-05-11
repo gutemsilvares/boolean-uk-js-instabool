08921f6d4419f73650f0d25548c2ace446247907
Agutembergues <gutemsilvares@hotmail.com>
2021-05-11 14:41:28 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,78 @@
 // write your code here
-const renderPosts = async () => {
-	let uri = "http://localhost:3000/comments";
-	const res = await fetch(uri);
-	const posts = await res.json();
-	console.log(comments);
 
-	let template = "";
-	comments.forEach((comment) => {
-		template +=
-			'<div class="comments"><h2>${comments.title}</h2><p><small>${comment.likes} likes</small></p><p>${comment.body.slice(0, 200)}</p><a href="/details.html"></a></div>';
+const imageContainer = document.querySelector(".image-container");
+
+fetch("http://localhost:3000/images")
+	.then(function (promise) {
+		let data = promise.json();
+		return data;
+	})
+	.then(function (data) {
+		for (const item of data) {
+			imageContainer.append(cardFunction(item));
+		}
 	});
-};
 
-window.addEventListener("DOMContentLoaded", () => renderPosts());
+// card
+function cardFunction(item) {
+	// Article section
+	const article = document.createElement("article");
+	article.classList = "image-card";
+
+	// h2
+	const h2 = document.createElement("h2");
+	h2.classList = item.title;
+
+	//image
+	const image = document.createElement("img");
+	image.src = item.image;
+	image.classList = "image";
+
+	// div section
+	const divSectionLikes = document.createElement("div");
+	divSectionLikes.classList = "likes-section";
+
+	// div section children
+	const divSpan = document.createElement("span");
+	divSpan.classList = "likes";
+	divSpan.innerText = item.likes;
+
+	const divButton = document.createElement("button");
+	divButton.classList = "likes-button";
+	divButton.innerText = "♥";
+
+	divSectionLikes.append(divSpan, divButton);
+
+	// Ul section
+	const ulSection = document.createElement("ul");
+	ulSection.classList = "comments";
+
+	for (const comment of item.comments) {
+		const listElement = document.createElement("li");
+		listElement.innerText = comment.content;
+		ulSection.append(listElement);
+	}
+
+	// form
+	const formSection = document.createElement("form");
+	formSection.classList = "comment-form";
+
+	// form input
+	const formInput = document.createElement("input");
+	formInput.classList = "comment-input";
+	formInput.setAttribute("type", "text");
+	formInput.setAttribute("name", "comment");
+	formInput.setAttribute("placeholder", "Add a comment...");
+
+	// form button
+	const formButton = document.createElement("button");
+	formButton.setAttribute("class", "comment-button");
+	formButton.setAttribute("type", "submit");
+	formButton.innerText = "Post";
+
+	formSection.append(formInput, formButton);
+
+	article.append(h2, image, divSectionLikes, ulSection, formSection);
+
+	return article;
+}
